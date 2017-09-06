47028582e9c1e654b35e59e4ed0a85b8b049dd88
Jean Carlos <gallegomosquerajeancarlos@gmail.com>
2017-09-06 08:41:20 -0500

    Fix chromium path, extract correct firmare file from working Cm-11 port

--- prebuilt/chromium/chromium_prebuilt.mk
+++ prebuilt/chromium/chromium_prebuilt.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/samsung/core33g/prebuilt/chromium
+LOCAL_PATH := device/samsung/vivalto3mveml3g/prebuilt/chromium
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/framework/webview/paks/am.pak:system/framework/webview/paks/am.pak \
